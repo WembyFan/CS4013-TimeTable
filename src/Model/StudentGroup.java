ca2928445171780a79ae934849d9bcc677bcf738
IanKennelly <24418013@studentmail.ul.ie>
2025-11-25 16:26:40 +0000

    Menu, TimetableManager syntax

--- src/Model/StudentGroup.java
+++ src/Model/StudentGroup.java
@@ -1,20 +1,23 @@
 package Model;
 
 public class StudentGroup {
+    private String course;
     private String groudId;
     private int year;
+    private int size;
 
-    public StudentGroup(String groupId, int year) {
+    public StudentGroup(String groupId, int year, int size, String course) {
         this.groudId = groupId;
         this.year = year;
+        this.size = size;
+        this.course = course;
+
     }
 
-    public String getGroupId() {
-        return groudId;
-    }
+    public String getCourse() { return course; }
 
-    public int getYear() {
-        return year;
-    }
+    public String getGroupId() { return groudId; }
+
+    public int getYear() { return year; }
 
 }
--- src/Model/TeachingAssignment.java
+++ src/Model/TeachingAssignment.java
@@ -1,4 +1,26 @@
 package Model;
 
 public class TeachingAssignment {
+    private String moduleCode;
+    private String classType;
+    private int maxCapacity;
+    private String roomType;
+    private String lecturerName;
+
+    public TeachingAssignment(String moduleCode, String classType, int maxCapacity, String roomType, String lecturerName) {
+        this.moduleCode = moduleCode;
+        this.classType = classType;
+        this.maxCapacity = maxCapacity;
+        this.roomType = roomType;
+        this.lecturerName = lecturerName;
+
+    }
+
+    public String getModuleCode() { return moduleCode; }
+    public String getClassType() { return classType; }
+    public int getMaxCapacity() { return maxCapacity; }
+    public String getRoomType() { return roomType; }
+    public String getLecturerName() { return lecturerName; }
+    public void setModuleCode(String moduleCode) { this.moduleCode = moduleCode; }
+
 }
