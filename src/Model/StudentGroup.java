81c4675532c91248f81015bf7688df89dd073a7c
IanKennelly <24418013@studentmail.ul.ie>
2025-11-25 21:21:28 +0000

    StudentGroup fix

--- src/Model/StudentGroup.java
+++ src/Model/StudentGroup.java
@@ -2,22 +2,35 @@ package Model;
 
 public class StudentGroup {
     private String course;
-    private String groudId;
+    private String groupId;
     private int year;
     private int size;
 
     public StudentGroup(String groupId, int year, int size, String course) {
-        this.groudId = groupId;
+        this.groupId = groupId;
         this.year = year;
         this.size = size;
         this.course = course;
 
     }
 
-    public String getCourse() { return course; }
+    public String getCourse() {
+        return course;
+    }
 
-    public String getGroupId() { return groudId; }
+    public String getGroupId() {
+        return groupId;
+    }
 
-    public int getYear() { return year; }
+    public int getYear() {
+        return year;
+    }
 
+    public int getSize() {
+        return size;
+    }
+
+    public int setSize() {
+        return size;
+    }
 }
